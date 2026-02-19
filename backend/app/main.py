2e5dec69b1409b0415751bbe08d347ba559cea01
aepii <simentalma@yahoo.com>
2026-02-18 22:33:58 -0600

    Implemented CORS middleware in the FastAPI backend.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.db import init_db
 from app.api.routes import auth, algs, images
 
@@ -15,6 +16,21 @@ async def lifespan(app: FastAPI):
 
 # FastAPI application
 app = FastAPI(title="CubeTutor API", lifespan=lifespan)
+
+# CORS
+origins = [
+    "http://localhost:8081",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Add routers
 app.include_router(auth.router)
